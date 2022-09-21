ca218e2c4177d8f598e092a4d75591f496de342f
Ankit Patel <ankitp5469@gmail.com>
2022-09-21 12:41:39 -0400

    Completed reviews carousel + fixed some menu styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NavBar from './components/NavBar/NavBar';
 import About from './components/About/About';
 import Hours from './components/Hours/Hours';
 import Directions from './components/Directions';
-import Reviews from './components/Reviews';
+import Reviews from './components/Reviews/Reviews';
 import Menu from './components/Menu/Menu';
 import Contact from './components/Contact'
 import topImage from './images/main1.svg'
--- src/components/Reviews.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Reviews = () => {
-  return (
-    <div className="Reviews" id="reviews">Reviews</div>
-  )
-}
-
-export default Reviews
