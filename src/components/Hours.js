6df8b66df921da800a51d2e893f8a1e52c765e45
Ankit Patel <ankitp5469@gmail.com>
2022-09-21 01:54:57 -0400

    Added hours & about

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Hours = () => {
   return (
-    <div className="Hours" id="hours">Hours</div>
+    <div className="Hours" id="hours">
+      <h1 className="Hours-heading">HOURS</h1>
+      <div className="Hours-columns">
+        <div className="Hours-breakdown">
+          <p className="Hours-paragraph">Monday</p>
+          <p className="Hours-paragraph">Tuesday-Thursday</p>
+          <p className="Hours-paragraph">Friday</p>
+          <p className="Hours-paragraph">Saturday</p>
+          <p className="Hours-paragraph">Sunday</p>
+        </div>
+        <div>
+          <p className="Hours-paragraph">Closed</p>
+          <p className="Hours-paragraph">11am-10pm</p>
+          <p className="Hours-paragraph">11am-11pm</p>
+          <p className="Hours-paragraph">noon-11pm</p>
+          <p className="Hours-paragraph">noon-10pm</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
