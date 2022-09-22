abdf7eb12d0435160043b6f8a0f88db3f5a73468
Aiden Bi <wenhaobi1999@hotmail.com>
2022-09-22 00:12:36 -0700

    added responsiveness

--- src/components/Hours/Hours.js
+++ src/components/Hours/Hours.js
@@ -6,21 +6,23 @@ const Hours = () => {
   return (
     <div className="Hours" id="hours">
       <img className='SpanningImage' src={SpanningImage}></img>
-      <h1 className="Hours-heading">HOURS</h1>
-      <div className="Hours-columns">
-        <div className="Hours-breakdown">
-          <p className="Hours-paragraph">Monday</p>
-          <p className="Hours-paragraph">Tuesday-Thursday</p>
-          <p className="Hours-paragraph">Friday</p>
-          <p className="Hours-paragraph">Saturday</p>
-          <p className="Hours-paragraph">Sunday</p>
-        </div>
-        <div>
-          <p className="Hours-paragraph">Closed</p>
-          <p className="Hours-paragraph">11am-10pm</p>
-          <p className="Hours-paragraph">11am-11pm</p>
-          <p className="Hours-paragraph">noon-11pm</p>
-          <p className="Hours-paragraph">noon-10pm</p>
+      <div className='HoursContainer'>
+        <h1 className="Hours-heading">HOURS</h1>
+        <div className="Hours-columns">
+          <div className="Hours-breakdown">
+            <p className="Hours-paragraph">Monday</p>
+            <p className="Hours-paragraph">Tuesday-Thursday</p>
+            <p className="Hours-paragraph">Friday</p>
+            <p className="Hours-paragraph">Saturday</p>
+            <p className="Hours-paragraph">Sunday</p>
+          </div>
+          <div>
+            <p className="Hours-paragraph">Closed</p>
+            <p className="Hours-paragraph">11am-10pm</p>
+            <p className="Hours-paragraph">11am-11pm</p>
+            <p className="Hours-paragraph">noon-11pm</p>
+            <p className="Hours-paragraph">noon-10pm</p>
+          </div>
         </div>
       </div>
     </div>
