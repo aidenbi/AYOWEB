68ceab4d606344d140f4226b597c3a48e37c198a
Ankit Patel <ankitp5469@gmail.com>
2022-09-22 00:48:36 -0400

    Added spanning image

--- src/App.js
+++ src/App.js
@@ -7,15 +7,19 @@ import Menu from './components/Menu/Menu';
 import Directions from './components/Directions/Directions';
 import Contact from './components/Contact'
 import TopCarousel from './components/TopCarousel/TopCarousel'
+import SpanningImage from './images/spanningFood.svg'
 
 function App() {
   return (
     <div>
       <NavBar></NavBar>
       <TopCarousel></TopCarousel>
-      <About></About>
-      <Hours></Hours>
-      <Directions></Directions>
+      <div className='SpanningContainer'>
+        <img className='SpanningImage' src={SpanningImage}></img>
+        <About></About>
+        <Hours></Hours>
+        <Directions></Directions>
+      </div>
       <Menu></Menu>
       <Reviews></Reviews>
       <Contact></Contact>
