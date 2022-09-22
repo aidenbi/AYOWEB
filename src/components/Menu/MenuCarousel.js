cac5a0596dd61af34141b52b8bd01132aaba310b
Alex Chin <alex.chin@fdmgroup.com>
2022-09-21 23:14:02 -0400

    working carousel

--- src/components/Menu/MenuCarousel.js
+++ src/components/Menu/MenuCarousel.js
@@ -13,7 +13,6 @@ import image2 from '../Menu/foodImage/Rectangle3.svg'
 import image3 from '../Menu/foodImage/Rectangle4.svg'
 import image4 from '../Menu/foodImage/Rectangle5.svg'
 import image5 from '../Menu/foodImage/Rectangle6.svg'
-import image6 from '../Menu/foodImage/Rectangle7.svg'
 
 
 export default class MenuCarousel extends Component {
@@ -23,10 +22,10 @@ export default class MenuCarousel extends Component {
             centerMode: true,
             infinite: true,
             centerPadding: "60px",
-            slidesToShow: 4,
-            speed: 500,
+            slidesToShow: 5,
+            speed: 1000,
             autoplay: true,
-            autoplaySpeed: 2000
+            autoplaySpeed: 1500
         };
 
         return (
