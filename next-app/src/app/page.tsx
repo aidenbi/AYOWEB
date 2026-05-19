5baceae4d77b6a7528daa8572be4f8012fe8d0e1
Aiden Bi <wenhaobi1999@hotmail.com>
2026-05-18 20:25:33 -0700

    switch about us and menu section positions

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -13,10 +13,10 @@ export default function Home() {
       <NavBar />
       <TopCarousel />
       <main className="content-shell">
-        <About />
+        <Menu />
         <Hours />
         <Directions />
-        <Menu />
+        <About />
         <Reviews />
         <Footer />
       </main>
